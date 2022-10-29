5a455705d01be7e123494c5ed810bbe6daf4d418
ctr0812 <andy0812@naver.com>
2022-10-29 23:06:41 +0900

    브루트 포스 문제풀이

---
+++ src/baekjoon/bruteforce/N2798.java
@@ -0,0 +1,29 @@
+package baekjoon.bruteforce;
+
+import java.util.Scanner;
+
+public class N2798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] <= m) {
+                        answer = Math.max(answer, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
